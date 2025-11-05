f5af0936e5c9ec572fde55b085c00c6c345e1aab
Ziyad Bin Ayoobkhan P <163715891+ZiyadBin@users.noreply.github.com>
2025-11-05 15:45:31 +0530

    Update ticketController.js

--- server/controllers/ticketController.js
+++ server/controllers/ticketController.js
@@ -1,1 +1,23 @@
+const ticketController = {
+    async saveTicket(req, res) {
+        // Save ticket logic
+    },
 
+    async getTickets(req, res) {
+        // Get tickets logic
+    },
+
+    async getTicket(req, res) {
+        // Get single ticket logic
+    },
+
+    async updateTicket(req, res) {
+        // Update ticket logic
+    },
+
+    async deleteTicket(req, res) {
+        // Delete ticket logic
+    }
+};
+
+module.exports = ticketController;
